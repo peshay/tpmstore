54d521c69051d6d2b862f847eb18719de64c4ee7
peshay <anhubert@gmail.com>
2019-01-17 16:44:47 +0100

    add support for python 3.7

--- setup.py
+++ setup.py
@@ -213,7 +213,8 @@ pkg_classifiers=[
     'Programming Language :: Python :: 3.3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6'
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7'
 ];
 pkg_keywords=[
     'ansible',
